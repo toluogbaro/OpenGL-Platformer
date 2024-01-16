903c612270f7e8d032cafd771ccee39dbdd950c6
TOLU OGBARO <AO231197@falmouth.ac.uk>
2024-01-16 06:15:07 +0000

    Finalised AI Implementation
    - Finished up Dynamic AI with wander and detection
    
    - Added bullet projectile

--- src/Application.cpp
+++ src/Application.cpp
@@ -26,6 +26,7 @@
 #include <string>
 #include <sstream>
 #include <memory>
+#include <random>
 
 
 #include "Renderer.h"
@@ -60,20 +61,21 @@ int windowWidth = 1080;
 
 
 std::unique_ptr<Camera> cameraObj(new Camera(50.0f, 45.0f)); //initialise camera with FOV
-static bool isGameStarted;
+
+static bool isPlayerAlive = true;
 static bool isGrounded;
+static bool isShooting;
+static bool isAIActive;
+static bool isWandering;
 
 
-
-World gameWorld;
-
-float playerRotX;
-float playerRotY;
 float walkSpeed = 10.0f; //player walkspeed stored as global to change when player is jumping
 float maxJumpHeight;
 
-enum PlayerState { NEUTRAL, JUMPING, RUNNING };
+enum PlayerState { NEUTRAL, JUMPING, RUNNING};
+enum AIStates {VERIFICATION, WANDER, ATTACK, DEATH };
 PlayerState currentPlayerState;
+AIStates currentAIState;
 
 //convert enum to string for use in imgui debugging
 const char* to_string(PlayerState p)
@@ -97,39 +99,42 @@ const char* to_string(PlayerState p)
             break;
 
     }
+
+    //credit to: https://mariusbancila.ro/blog/2023/08/17/how-to-convert-an-enum-to-string-in-cpp/
+}
+
+const char* to_string(AIStates ai)
+{
+    switch (ai)
+    {
+    case  AIStates::VERIFICATION:
+        return "Verification";
+        break;
+
+    case  AIStates::WANDER:
+        return "Wander";
+        break;
+
+    case  AIStates::ATTACK:
+        return "Attack";
+        break;
+
+    case  AIStates::DEATH:
+        return "Death";
+        break;
+
+    default:
+        return "None";
+        break;
+
+    }
+
+    //credit to: https://mariusbancila.ro/blog/2023/08/17/how-to-convert-an-enum-to-string-in-cpp/
 }
 
 void mouse_callback(GLFWwindow* currentWindow, double xPos, double yPos) 
 {
     cameraObj->ProcessMouseInput(currentWindow, xPos, yPos, windowHeight, windowWidth);
-
-    float lastMouseX = 0;
-    float lastMouseY = 0;
-
-   
-    float xOffset = xPos - lastMouseX;
-    float yOffset = lastMouseY - yPos;
-
-
-    //current mouse input vs the last mouse input
-
-    lastMouseX = xPos;
-    lastMouseY = yPos;
-
-    //setting the last mouse positions to the current ones
-
-    xOffset *= 0.1f;
-    yOffset *= 0.1f;
-
-    //multiplying offset by a sensitivity value, for ex. 0.5f, dampens it
-
-    playerRotX += xOffset;
-    playerRotY += yOffset;
-
-    // adding the current mouse position - the last mouse position gives you where the mouse is currently meant
-    // to be in terms of these euler angles
-
-    yOffset = glm::clamp(yOffset, -89.0f, 89.0f);
 
 }
 
@@ -202,6 +207,15 @@ void ImGuiRun(ImGuiIO& io)
 
         ImGui::Text("Player State = %s",  to_string(currentPlayerState));
 
+        ImGui::Text("AI State = %s",  to_string(currentAIState));
+
+
+        if (ImGui::Button("Activate AI", standardButtonSize))
+        {
+            isAIActive = true;
+            currentAIState = VERIFICATION;
+        }
+
         if (ImGui::Button("Free Roam", standardButtonSize))
         {
             cameraObj->ChangeCameraMode(Camera_Mode::FREE_ROAM);
@@ -210,11 +224,6 @@ void ImGuiRun(ImGuiIO& io)
         if (ImGui::Button("Player Mode", standardButtonSize))
         {
             cameraObj->ChangeCameraMode(Camera_Mode::PLAYER);
-        }
-
-        if (ImGui::Button("Start Game", standardButtonSize))
-        {
-            isGameStarted = true;
         }
 
 
@@ -248,6 +257,24 @@ bool CollisionDetection(glm::vec3 positionOne, glm::vec3 sizeOne, glm::vec3 posi
     return collisionX && collisionY && collisionZ;
     //just like in traditional AABB, collision is detected when all three axis are colliding
 }
+
+bool DistanceCheck(glm::vec3 positionOne, glm::vec3 positionTwo, float distanceThreshold)
+{
+    glm::vec3 distBetweenObj = positionTwo - positionOne;
+
+    float distBeforeSqrX = (distBetweenObj.x * distBetweenObj.x);
+    float distBeforeSqrY = (distBetweenObj.y * distBetweenObj.y);
+    float distBeforeSqrZ = (distBetweenObj.z * distBetweenObj.z);
+
+    float distBeforeSqr = distBeforeSqrX + distBeforeSqrY + distBeforeSqrZ;
+
+    float distanceSquared = glm::sqrt(distBeforeSqr);
+
+    bool detection = distanceSquared <= distanceThreshold ? true : false;
+
+    return detection;
+}
+
 
 
 int main(void)
@@ -320,7 +347,7 @@ int main(void)
 
         ImGuiIO& io = ImGui::GetIO();
 
-        glm::vec3 originalPos = glm::vec3(0.0f);
+        glm::vec3 originalPos = glm::vec3(0.0f, 0.0f, -10.0f);
         glm::vec3 playerTransform = originalPos;
         
         glm::vec3 platformPos = glm::vec3(0.0f);
@@ -331,13 +358,25 @@ int main(void)
         glm::vec3 normalVec = playerTransform + dirPlayerToPlatform;
         //normalised vector between player and platform
 
-        float oldTimeSinceStart = 0.0f;
-        float timeSinceStart = 0.0f;
-        float airTime = 0.1f;
+        float oldTimeSinceStart = 0.0f; //last frame
+        float timeSinceStart = 0.0f; //current frame
+        float airTime = 0.1f;//how long the player stays in the air
         
-        float gravityVelo = 0.0f;
-        float gravityAccel = 0.0f;
-        float jumpDistance = 0.0f;
+        float gravityVelo = 0.0f; //gravity velocity
+        float gravityAccel = 0.0f; //gravity acceleration
+        float jumpPower = 3.5f;
+
+        glm::vec3 originalBulletPos = glm::vec3(0.0f);
+        glm::vec3 bulletPosition = glm::vec3(0.0f);
+        float currentBulletPoz = originalBulletPos.z;
+        glm::vec3 bulletSize = glm::vec3(0.2f);
+
+        glm::vec3 aiPosition = glm::vec3(0, -8.6f, 10.0f);
+        glm::vec3 aiSize = glm::vec3(1.0f);
+        float wanderTimer = 0.0f;
+        int randDistX = 0;
+        int randDistZ = 0;
+        bool PositiveOrNegative = false;
 
 #pragma endregion
 
@@ -349,6 +388,7 @@ int main(void)
             oldTimeSinceStart = timeSinceStart;
 
             renderer->Clear();
+            
 
             ProcessInput(window);
             glfwSetKeyCallback(window, key_callback);
@@ -357,6 +397,7 @@ int main(void)
 
             if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && isGrounded)
             {
+                
                 currentPlayerState = JUMPING;
                 //keep player state as jump for each frame cycle
 
@@ -364,25 +405,22 @@ int main(void)
 
             switch (currentPlayerState)
             {
-            case NEUTRAL:
+                case NEUTRAL:
 
                 break;
 
-            case JUMPING:
+                case JUMPING:
                 if (playerTransform.y < maxJumpHeight)
                 {
-                    playerTransform.y += 6.0f * deltaTime;
+                    playerTransform.y += jumpPower * deltaTime;
                     //addition each frame
                 }
-                break;
-
-            default:
                 break;
             }
 
             if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
             {
-                //if (isColliding) playerTransform.z += (normalVec.z * 5.0f) * deltaTime;
+                //if (isGrounded) playerTransform.z += (normalVec.z * 3.0f) * deltaTime;
 
                 playerTransform.z += walkSpeed * deltaTime;
 
@@ -390,7 +428,7 @@ int main(void)
 
             if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
             {
-                //if (isColliding) playerTransform.z -= (normalVec.z * 5.0f) * deltaTime;
+                //if (isGrounded) playerTransform.z += (normalVec.z * 3.0f) * deltaTime;
                 playerTransform.z -= walkSpeed * deltaTime;
 
             }
@@ -398,18 +436,19 @@ int main(void)
             if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
             {
 
-                //if (isColliding) playerTransform.x -= (normalVec.x * 5.0f) * deltaTime;
+                //if (isGrounded) playerTransform.x += (normalVec.x * 3.0f) * deltaTime;
                 playerTransform.x -= walkSpeed * deltaTime;
 
             }
 
             if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
             {
-                //if (isColliding) playerTransform.x += (normalVec.x * 5.0f) * deltaTime;
+                //if (isGrounded) playerTransform.x += (normalVec.x * 3.0f) * deltaTime;
 
                 playerTransform.x += walkSpeed * deltaTime;
 
             }
+
 
 
             if (!isGrounded)
@@ -417,12 +456,13 @@ int main(void)
                 airTime -= deltaTime;
                 //airtime goes down while in the air
 
-                walkSpeed = 2.0f;
+                walkSpeed = 3.0f;
                 //walk speed slowed down to simulate lack of control while jumping
                 
-                gravityVelo += -9.81f * deltaTime;
-                gravityAccel += gravityVelo * deltaTime;
-                //gravity increases over time
+                 gravityVelo += -7.00f * deltaTime;
+                 gravityAccel += gravityVelo * deltaTime;
+                //gravity velocity is equal to gravitational constant over time
+                 //gravity acceleration is equal to gravity velocity over time
 
                 if (airTime <= 0)
                 {
@@ -433,24 +473,36 @@ int main(void)
             }
             else
             {
+                
                 airTime = 0.1f;
                 //reset amount of time player is allowed to be in the air before gravity kicks in
-                walkSpeed = 10.0f;
-                //reset walk speed
-                maxJumpHeight = playerTransform.y + 5.0f;
-                //calculate max jump height only when player is grounded to avoid infinite jump
-                currentPlayerState = NEUTRAL;
-                //reset player state
+
                 gravityVelo = 0.0f;
                 gravityAccel = 0.0f;
                 //reset gravity values for acceleration after object has been in the air for a set amount of time
+
+                maxJumpHeight = playerTransform.y + 6.0f;
+                //calculate max jump height only when player is grounded to avoid infinite jump
+
+                walkSpeed = 10.0f;
                
             }
 
 #pragma endregion
 
+            if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+            {
+                isShooting = true;
+            }
+            else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE)
+            {
+                isShooting = false;
+            }
+
+
             #pragma region Matrices
 
+          
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, playerTransform);
             //model matrix updates based on current player transform
@@ -475,20 +527,149 @@ int main(void)
 
             //updating the shader with the defined MVP values
 
-            renderer->DrawObject(*va, *ib); //draw call using Vertex Array and Index Buffer
+            if (isPlayerAlive)
+            {
+                renderer->DrawObject(*va, *ib);
+            }
+                //draw call using Vertex Array and Index Buffer
 
             glm::mat4 platformModel = glm::mat4(1.0f);
             platformModel = glm::translate(platformModel, platformPos);
-            glm::vec3 platformSize = glm::vec3(10.0f);
+            glm::vec3 platformSize = glm::vec3(24.0f, 12.0f, 36.0f);
             platformModel = glm::scale(platformModel, platformSize);
 
             shader->SetUniform4f("u_Colour", 0.0f, 0.6f, 0.8f, 1.0f);
             shader->SetUnifromMat4("model", platformModel);
-            shader->SetUnifromMat4("view", cameraObj->ProcessViewMatrix());
-            shader->SetUnifromMat4("projection", projection);
-            shader->Bind();
 
             renderer->DrawObject(*va, *ib);
+
+            if (isShooting)
+            {
+                currentBulletPoz += 10.0f * deltaTime;
+
+                glm::mat4 bulletModel = glm::mat4(1.0f);
+                
+                bulletPosition = glm::vec3(originalBulletPos.x, originalBulletPos.y, currentBulletPoz);
+
+                bulletModel = glm::translate(bulletModel, bulletPosition);
+                bulletModel = glm::scale(bulletModel, bulletSize);
+
+                shader->SetUniform4f("u_Colour", 0.5f, 1.0f, 0.8f, 1.0f);
+                shader->SetUnifromMat4("model", bulletModel);
+
+                renderer->DrawObject(*va, *ib);
+
+            }
+            else
+            {
+                originalBulletPos = glm::vec3(playerTransform.x, playerTransform.y, playerTransform.z + 0.6f);
+                currentBulletPoz = originalBulletPos.z;
+            }
+
+
+          
+
+            if (isAIActive)
+            {
+                std::random_device rd;
+                std::mt19937 gen(rd());
+                std::uniform_int_distribution<> distr(-30, 30);
+                
+                glm::vec3 oldAIPos = glm::vec3(0.0f);
+
+                switch (currentAIState)
+                {
+                case VERIFICATION:
+                    wanderTimer += deltaTime;
+                    oldAIPos = glm::vec3(aiPosition.x, aiPosition.y, aiPosition.z);
+                    randDistX = distr(gen);
+                    randDistZ = distr(gen);
+                    PositiveOrNegative = (rand() % 100) < 50;
+                    break;
+
+                case WANDER:
+                    wanderTimer -= deltaTime;
+                    glm::vec3 maxDist = glm::vec3(0.0f);
+
+                    if(PositiveOrNegative)
+                        maxDist =  glm::vec3(oldAIPos.x + randDistX, 0, oldAIPos.z - randDistZ);
+                    else
+                        maxDist = glm::vec3(oldAIPos.x - randDistX, 0, oldAIPos.z + randDistZ);
+
+                    glm::vec3 dirToMaxDist = glm::normalize(maxDist);
+
+                    aiPosition += dirToMaxDist * 3.5f * deltaTime;
+
+                    break;
+
+                case ATTACK:
+
+                    glm::vec3 aiToPlayer = playerTransform - aiPosition;
+                    glm::vec3 dirAItoPlayer = glm::normalize(aiToPlayer);
+                    
+                    std::cout << isPlayerAlive << std::endl;
+
+                    if (aiPosition != playerTransform )
+                    {
+                        aiPosition += dirAItoPlayer * 6.0f * deltaTime;
+                       
+                    }
+                    else
+                    {
+                        isPlayerAlive = false;
+                    }
+                    break;
+
+                }
+
+                if (wanderTimer <= 0.0f)
+                {
+                    currentAIState = VERIFICATION;
+
+                }
+                else if(wanderTimer > 1.0f)
+                {
+                    currentAIState = WANDER;
+                }
+
+
+                glm::mat4 aiModel = glm::mat4(1.0f);
+                aiModel = glm::translate(aiModel, aiPosition);
+
+                shader->SetUniform4f("u_Colour", 1.0f, 1.0f, 0.0f, 1.0f);
+                shader->SetUnifromMat4("model", aiModel);
+
+                renderer->DrawObject(*va, *ib);
+
+                glm::mat4 aiDetectionGizmo = glm::mat4(1.0f);
+                aiDetectionGizmo = glm::translate(aiDetectionGizmo, glm::vec3(aiPosition.x, aiPosition.y + 2.5f, aiPosition.z));
+                aiDetectionGizmo = glm::scale(aiDetectionGizmo, glm::vec3(12.0f, 5.0f, 12.0f));
+                shader->SetUniform4f("u_Colour", 1.0f, 0.0f, 0.0f, 1.0f);
+                shader->SetUnifromMat4("model", aiDetectionGizmo);
+
+                if (DistanceCheck(playerTransform, aiPosition, 7.5f))
+                {
+                    shader->SetUniform4f("u_Colour", 0.0f, 1.0f, 0.0f, 1.0f);
+                    currentAIState = ATTACK;
+                }
+                else if (!DistanceCheck(playerTransform, aiPosition, 7.5f) && currentAIState != WANDER)
+                {
+                    currentAIState = VERIFICATION;
+                }
+
+                glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+                renderer->DrawObject(*va, *ib);
+
+                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+                if (DistanceCheck(aiPosition, bulletPosition, 0.5f))
+                {
+                    currentAIState = DEATH;
+                    isAIActive = false;
+                }
+                
+            }
 
 #pragma endregion
 
@@ -501,7 +682,6 @@ int main(void)
 
             //calculating the direction between player and platform for collision on the xz plane
             //to add an equal force that sends the player back
-            //simulating inelastic collision that keeps the player static
 
             if (CollisionDetection(playerTransform, glm::vec3(1.0f), platformPos, platformSize))
             {
@@ -510,26 +690,20 @@ int main(void)
                 lightModel = glm::scale(lightModel, glm::vec3(1.0f));
                 shader->SetUniform4f("u_Colour", 1.0f, 1.0f, 1.0f, 1.0f);
                 shader->SetUnifromMat4("model", lightModel);
-                shader->SetUnifromMat4("view", cameraObj->ProcessViewMatrix());
-                shader->SetUnifromMat4("projection", projection);
-                shader->Bind();
 
                 renderer->DrawObject(*va, *ib);
 
                 //rendering a bright object for debugging purposes
 
+                currentPlayerState = NEUTRAL;
+                //reset player state
+
                 isGrounded = true;
 
-               // std::cout << "Collided" << std::endl;
+                //std::cout << "Collided" << std::endl;
             }
             else
                 isGrounded = false;
-
-
-            if (isGameStarted)
-            {
-                gameWorld.Update(deltaTime);
-            }
 
             /* run UI Window */
             ImGuiRun(io);
